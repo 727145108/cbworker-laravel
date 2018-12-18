34d3485062ac7dd33fbebd821d1e44bca3ef62dd
crababy <727145108@qq.com>
2018-12-18 13:55:14 +0800

    update redis alias

--- src/Core/Application.php
+++ src/Core/Application.php
@@ -196,6 +196,7 @@ class Application extends Container implements ApplicationContract
                'db' => [\Illuminate\Database\DatabaseManager::class],
                'db.connection' => [\Illuminate\Database\Connection::class, \Illuminate\Database\ConnectionInterface::class],
                'events' => [\Cbworker\Core\Events\Dispatcher::class, \Illuminate\Contracts\Events\Dispatcher::class],
+               'redis'  => [\Cbworker\Core\Redis\Database::class, Illuminate\Contracts\Redis\Database::class],
              ] as $key => $aliases) {
       foreach ($aliases as $alias) {
         $this->alias($key, $alias);
